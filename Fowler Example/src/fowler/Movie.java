aa8752a769fc15fbc72da8dc30b5e53ee5934674
quint2007 <quint0095@gmail.com>
2016-05-02 22:57:12 +0200

    Use Self Encapsulate Field, Create Price class

--- Fowler Example/src/fowler/Movie.java
+++ Fowler Example/src/fowler/Movie.java
@@ -5,15 +5,28 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
     private String title;
     private int priceCode;
+    private Price price;
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
-        priceCode = newpriceCode;
+        setPriceCode(newpriceCode);
     }
     public int getPriceCode() {
-        return priceCode;
+        return price.getPriceCode();
     }
     public void setPriceCode(int arg) {
-        priceCode = arg;
+        switch (arg){
+        case REGULAR:
+        	price = new RegularPrice();
+        	break;
+        case CHILDRENS:
+        	price = new ChildrensPrice();
+        	break;
+        case NEW_RELEASE:
+        	price = new NewReleasePrice();
+        	break;
+        	default: 
+        		throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
     public String getTitle (){
         return title;
@@ -21,15 +34,15 @@ public class Movie {
 	public double getCharge(int daysRented) {
 		double result = 0;
         switch (getPriceCode()) {
-            case Movie.REGULAR:
+            case REGULAR:
             	result += 2;
                 if (daysRented > 2)
                 	result += (daysRented - 2) * 1.5;
                 break;
-            case Movie.NEW_RELEASE:
+            case NEW_RELEASE:
             	result += daysRented * 3;
                 break;
-            case Movie.CHILDRENS:
+            case CHILDRENS:
             	result += 1.5;
                 if (daysRented > 3)
                 	result += (daysRented - 3) * 1.5;
