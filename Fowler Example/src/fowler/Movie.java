cc9b660a6de9a9f632f3efa8079c3d37b5effa66
quint2007 <quint0095@gmail.com>
2016-05-02 22:58:57 +0200

    Move Method getCharge to Price

--- Fowler Example/src/fowler/Movie.java
+++ Fowler Example/src/fowler/Movie.java
@@ -32,23 +32,7 @@ public class Movie {
         return title;
     }
 	public double getCharge(int daysRented) {
-		double result = 0;
-        switch (getPriceCode()) {
-            case REGULAR:
-            	result += 2;
-                if (daysRented > 2)
-                	result += (daysRented - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-            	result += daysRented * 3;
-                break;
-            case CHILDRENS:
-            	result += 1.5;
-                if (daysRented > 3)
-                	result += (daysRented - 3) * 1.5;
-                break;
-        }
-        return result;
+		return price.getCharge(daysRented);
 	}
 	public int getFrequentRenterPoints(int daysRented) {
 		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) 
