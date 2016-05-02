a47ecc7f84c8d3b9bc6e9f3706be9ddc86c9d761
quint2007 <quint0095@gmail.com>
2016-05-02 20:47:45 +0200

    Finish implementing Test

--- Fowler Example/src/test/FowlerTest.java
+++ Fowler Example/src/test/FowlerTest.java
@@ -23,14 +23,15 @@ public class FowlerTest {
 		r1 = new Rental(m1, 7);
 		r2 = new Rental(m2, 10);
 		r3 = new Rental(m3, 2);
+		customer = new Customer("Peter");
 		customer.addRental(r1);
 		customer.addRental(r2);
-		customer.addRental(r2);
+		customer.addRental(r3);
 
 	}
 	@Test
 	public void testStatement() {
-		assertEquals("bla", customer.statement());
+		assertEquals("Rental Record for Peter\n\tTitle\t\tDays\tAmount\n\tStar Wars I\t\t7\t0.0\n\tCloud Atlas\t\t10\t0.0\n\tSpectre\t\t2\t0.0\nAmount owed is 0.0\nYou earned 3 frequent renter points", customer.statement());
 	}
 
 }
